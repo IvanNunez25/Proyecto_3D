94461461163fdf8394a64f298444009a8eefb42f
Ivanovicx Nuñez <ivan25606@gmail.com>
2023-12-16 10:46:34 -0600

    Initial Commit

---
+++ Proyecto_3D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto_3D
+//
+//  Created by Ivanovicx Nuñez on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
